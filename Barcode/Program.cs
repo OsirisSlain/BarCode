39d0e1f5c0598e70ea3f385f62d63520e2fb9fda
Blake Shaw <Blake.Shaw@gmail.com>
2017-02-19 16:25:54 -0800

    Making margins better

--- Barcode/Program.cs
+++ Barcode/Program.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
-using System.Text;
 
 namespace Barcode
 {
     static class Program
     {
-        static string quiet = "00000000000000000000";
+        static string quiet = "000000000000";
         static string guard = "101";
         static string middle = "01010";
         static string[] leftEncoding = new[]
@@ -29,26 +27,34 @@ namespace Barcode
 
         static void Main(string[] args)
         {
-            var wat = GenerateUpcA("123456789012");
+            var wat = GenerateUpcA("045496891978");
             wat.Save("test.png", ImageFormat.Png);
         }
 
-        static Bitmap GenerateUpcA(string upc)
+        static Bitmap GenerateUpcA(string upc, int scale = 4)
         {
             var cleanUpc = upc.Replace("-", "").Replace(" ", "");
             var leftDigits = cleanUpc.Take(6).Select(x => x - '0').GenerateBarString(leftEncoding);
             var rightDigits = cleanUpc.Skip(6).Select(x => x - '0').GenerateBarString(rightEncoding);
             var fullCode = quiet + guard + leftDigits + middle + rightDigits + guard + quiet;
 
-            var bars = new Bitmap(135, 84);
-            for (int x = 0; x < bars.Width; x++)
+            int width = fullCode.Length * scale;
+            int height = (int)(width * 0.618);
+            int margin = height / 7;
+            var bars = new Bitmap(width, height);
+
+            for (int x = 0; x < width; x++)
             {
-                var color = fullCode[x] == '0' ? Color.White : Color.Black;
-                for (int y = 0; y < bars.Height; y++)
+                var color = fullCode[x/scale] == '0' ? Color.White : Color.Black;
+                for (int y = 0; y < height; y++)
                 {
-                    bars.SetPixel(x, y, color);
+                    if (y < margin || y > (height - margin))
+                        bars.SetPixel(x,y,Color.White);
+                    else
+                        bars.SetPixel(x, y, color);
                 }
             }
+
             return bars;
         }
     }
