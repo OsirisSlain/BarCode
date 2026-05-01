50b7cd00326c9d1e321f9ec954feb99fdd665f75
Blake Shaw <blake.shaw@gmail.com>
2026-05-01 17:06:20 -0600

    Upgrading to dotnet 10

--- Barcode/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Barcode")]
-[assembly: AssemblyDescription("A barcode generator")]
-[assembly: AssemblyCompany("Blake Shaw")]
-[assembly: AssemblyProduct("Barcode")]
-[assembly: AssemblyCopyright("Copyright © Blake Shaw 2017")]
-[assembly: AssemblyVersion("1.0.1.*")]
